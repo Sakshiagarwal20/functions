4359e1f2f2bfc5e0a86e695c0f919a6f88ee8b6f
Sakshi <sakshi8125@gmail.com>
2020-03-09 19:47:30 +0530

    patterns

---
+++ patterns/dog.java
@@ -0,0 +1,17 @@
+ public class Dog implements Animal {
+    public void eat() {
+        System.out.println("Dog is eating, woof!");
+    }    
+}
+
+public class Cat implements Animal {
+    public void eat() {
+        System.out.println("Cat is eating, meow!");
+    }   
+}
+
+public class Rabbit implements Animal {
+    public void eat() {
+        System.out.println("Rabbit is eating, squeak!");
+    } 
+}
